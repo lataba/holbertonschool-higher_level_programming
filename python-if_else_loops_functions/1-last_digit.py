0e6dfaddb7c48f9c97b6b4cbb4faaff1f2a721c9
lataba <ltballeste@gmail.com>
2023-05-30 20:06:40 -0700

    fixing the print line

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 last = abs(number) % 10
-print("Last digit of", '{:d}'.format(number), "is", '{:d}'.format(last), end=" ")
+
+print("Last digit of", '{:d}'.format(number), "is",
+        '{:d}'.format(last), end=" ")
+
 if last > 5:
     print("and is greater than 5")
 elif (last < 6) and (last != 0):
