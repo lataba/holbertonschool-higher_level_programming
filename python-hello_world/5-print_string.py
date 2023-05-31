26a1eb91c3320dd434c70013cfc29e628562eba5
lataba <ltballeste@gmail.com>
2023-05-30 21:12:07 -0300

    adding documentation

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
 """
 Print 3 times a string stored in the variable str,
 followed by its first 9 characters.
 """
+#!/usr/bin/python3
 str = "Holberton School"
 print(f"{str}" * 3)
 print(str[:9])
