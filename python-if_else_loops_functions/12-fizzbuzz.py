f14a2daeb1b08eca90d2ef5324ced12dae32c2fd
lataba <ltballeste@gmail.com>
2023-06-04 18:03:15 -0700

    task12 adding format

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,10 +4,10 @@ def fizzbuzz():
 
     for number in range(1, 101):
         if ((number % 3 == 0) and (number % 5 == 0)):
-            print("FizzBuzz", end=' ')
+            print(f"FizzBuzz", end=' ')
         elif number % 5 == 0:
-            print("Buzz", end=' ')
+            print(f"Buzz", end=' ')
         elif number % 3 == 0:
-            print("Fizz", end=' ')
+            print(f"Fizz", end=' ')
         else:
             print("{}".format(number), end=' ')
