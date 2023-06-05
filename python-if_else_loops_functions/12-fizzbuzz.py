491a7675a4588dacd112233d1f6cbb6d7214a165
lataba <ltballeste@gmail.com>
2023-06-04 18:00:23 -0700

    task12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def fizzbuzz():
+
+    for number in range(1, 101):
+        if ((number % 3 == 0) and (number % 5 == 0)):
+            print("FizzBuzz", end=' ')
+        elif number % 5 == 0:
+            print("Buzz", end=' ')
+        elif number % 3 == 0:
+            print("Fizz", end=' ')
+        else:
+            print("{}".format(number), end=' ')
