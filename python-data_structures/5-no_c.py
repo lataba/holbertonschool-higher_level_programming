89400fd8fe1f271dff60a76b58e0a58f97419789
lataba <ltballeste@gmail.com>
2023-06-06 19:36:32 -0700

    task5 changing the new_string upper to the if condition

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,11 +2,11 @@
 
 def no_c(my_string):
 
+    new_string = ''
+
     if my_string:
-
-        new_string = ''
-
         for x in my_string:
             if x != 'c' and x != 'C':
                 new_string = new_string + x
-        return (new_string)
+
+    return (new_string)
