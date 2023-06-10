5c0cd6aa27448616d9db110bd2bb6fbb90a27da6
root <ltballeste@gmail.com>
2023-06-10 05:09:15 -0700

    removing existance condition

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,13 +3,12 @@
 def search_replace(my_list, search, replace):
     """function that replaces all occurrences of an element in a list"""
 
-    if my_list:
-        new_list = my_list[:]
-        i = 0
+    new_list = my_list[:]
+    i = 0
 
-        for elem in new_list:
-            i = i + 1
-            if elem == search:
-                new_list[i] = replace
+    for elem in new_list:
+        i = i + 1
+        if elem == search:
+            new_list[i] = replace
 
     return (new_list)
