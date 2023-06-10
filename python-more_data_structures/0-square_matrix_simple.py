5c1b13d1053e464ff63a07c3b663f9dc3a9becbd
root <ltballeste@gmail.com>
2023-06-10 04:44:39 -0700

    removing documentation comentary

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -10,8 +10,8 @@ def square_matrix_simple(matrix=[]):
         new_row = []
 
         for i in row:
-            new_element = i*i
+            new_element = i * i
             new_row.append(new_element)
         result.append(new_row)
-           
+
     return (result)
