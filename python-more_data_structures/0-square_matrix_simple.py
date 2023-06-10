331bf2e595f24df5ec164edfce512548b8e89682
root <ltballeste@gmail.com>
2023-06-09 19:03:26 -0700

    task 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    result = []
+        
+    for row in matrix:
+        new_row = []
+
+        for i in row:
+            new_element = i*i
+            new_row.append(new_element)
+        result.append(new_row)
+           
+    return (result)        
