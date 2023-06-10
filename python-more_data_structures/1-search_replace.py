7e0fb5b3c2f06ad0ad81e5f6dd893bf79ce03e36
root <ltballeste@gmail.com>
2023-06-10 05:07:35 -0700

    change code to replace all occurrence of an element searched

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,14 +3,13 @@
 def search_replace(my_list, search, replace):
     """function that replaces all occurrences of an element in a list"""
 
-    new_list = my_list[:]
-    i = 0
+    if my_list:
+        new_list = my_list[:]
+        i = 0
 
-    for elem in new_list:
-        i = i + 1
-        if elem == search:
-            break
-
-    new_list[i] = replace
+        for elem in new_list:
+            i = i + 1
+            if elem == search:
+                new_list[i] = replace
 
     return (new_list)
