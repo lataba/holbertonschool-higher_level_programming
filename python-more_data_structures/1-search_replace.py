53dacc6b10df5e122be8a802ce00456f91973a01
root <ltballeste@gmail.com>
2023-06-10 05:04:14 -0700

    task 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """function that replaces all occurrences of an element in a list"""
+
+    new_list = my_list[:]
+    i = 0
+
+    for elem in new_list:
+        i = i + 1
+        if elem == search:
+            break
+
+    new_list[i] = replace
+
+    return (new_list)
