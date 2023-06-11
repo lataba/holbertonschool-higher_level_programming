50e503b48f331a5d32b50762a9f7848bf6335b81
root <ltballeste@gmail.com>
2023-06-10 21:28:33 -0700

    reduce lines of the file to 3

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    """function that returns a list with all values multiplied"""
-
-    new_list = list(map(lambda x: x * number, my_list))
-
-    return (new_list)
+    return (list(map(lambda x: x * number, my_list)))
