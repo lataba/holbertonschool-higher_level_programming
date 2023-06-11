c75954a7a0f7b2f0bc903740c33de14eed1a8966
root <ltballeste@gmail.com>
2023-06-10 18:32:38 -0700

    task5

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """function that returns the number of keys in a dictionary"""
+
+    count = 0
+
+    for i in a_dictionary:
+        count = count + 1
+
+    return (count)
