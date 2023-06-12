e3f3f754fef3e32f023cf0bc557eefd31b98b340
root <ltballeste@gmail.com>
2023-06-11 17:24:23 -0700

    task1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """function that prints an integer"""
+
+    try:
+        print("{:d}".format(value))
+
+    except (ValueError, TypeError):
+        return (False)
+
+    return (True)
