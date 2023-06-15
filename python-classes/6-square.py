84af5ce02ca4505ab62279c9911729e331f130c6
root <ltballeste@gmail.com>
2023-06-15 12:17:01 -0700

    task6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
     """Class that defines a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -47,4 +47,4 @@ class Square:
             for i in range(self.__position[1]):
                 print('')
             for j in range(self.__size):
-                print(' ' * self.position[0] + "#" * self.__size)
+                print(' ' * self.position[0] + '#' * self.__size)
