328239fc598d6afff221fd5bc9a81eb042742b9b
root <ltballeste@gmail.com>
2023-06-17 14:53:41 -0700

    adding a blank line (7)

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,7 @@
 
 def add_integer(a, b=98):
     """Definition of the function"""
+
     if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
     if (not isinstance(b, int) and not isinstance(b, float)):
