e138b4233a110beca3f0c560b5c3d16ab3164ad0
root <ltballeste@gmail.com>
2023-06-17 20:04:29 -0700

    changes at test 7 TypeError and change the if condition on module

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,7 +9,7 @@ def print_square(size):
     Function that prints a square with the character #
     """
 
-    if not type(size) == int:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
 
     if size < 0:
