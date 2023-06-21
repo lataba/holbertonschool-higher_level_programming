ba0e945f3ac3d5b070185190c343531261a86433
root <ltballeste@gmail.com>
2023-06-20 18:13:35 -0700

    tasks 0-28

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines a function that copies a list
+"""
+
+
+def copy_list(a_list):
+    """function that returns a copy of a list"""
+
+    new_list = a_list[:]
+
+    return new_list
