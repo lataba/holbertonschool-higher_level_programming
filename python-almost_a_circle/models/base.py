1f0351b0da28e30733f6dccb26230cba8ce99276
root <ltballeste@gmail.com>
2023-06-29 12:58:29 -0700

    models and test directories

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Class definition
+"""
+
+
+class Base:
+    """Base class definition"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Unittest for ....
+"""
+
+import unittest
+from models.base import Base
+
+
+class 
