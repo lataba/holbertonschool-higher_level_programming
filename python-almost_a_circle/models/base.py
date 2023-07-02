350bf16712b73aaf9f989bcf4b8a199579508aa9
root <ltballeste@gmail.com>
2023-07-02 11:17:24 -0700

    base adding Dictionary to Instance and File to instances

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,6 +38,28 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation"""
-        if json_string is None or json_string == []:
+        if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes"""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                dummy = cls(1, 1)
+            else:
+                dummy = cls(1)
+            dummy.update(**dictionary)
+            return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**dic) for dic in list_dicts]
+        except IOError:
+            return []
