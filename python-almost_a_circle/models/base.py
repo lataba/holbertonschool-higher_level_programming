7cded581a552ea2164391eb83d5d7a3a9e95d542
root <ltballeste@gmail.com>
2023-07-01 21:18:03 -0700

    changing to_json_string if declaration

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries == "None" or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return []
         return json.dumps(list_dictionaries)
 
