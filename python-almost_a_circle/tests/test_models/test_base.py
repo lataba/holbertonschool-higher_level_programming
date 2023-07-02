da70b69933c7f61839b0fdeaa3fcb88ae2eead6b
root <ltballeste@gmail.com>
2023-07-02 13:32:45 -0700

    adding base tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -27,11 +27,28 @@ class TestBase(unittest.TestCase):
         b2 = Base()
         self.assertEqual(b1.id, b2.id - 1)
 
+    def test_nb_instances_private(self):
+        """Teste nb_instances"""
+        with self.assertRaises(AttributeError):
+            print(Base(12).__nb_instances)
+
     def test_public_id(self):
         """Test that the "id" attribute is correctly set when provided"""
         b1 = Base(3)
         b1.id = 8
         self.assertEqual(b1.id, 8)
+
+    def test_str_id(self):
+        """Teste str id"""
+        self.assertEqual("hello", Base("hello").id)
+
+    def test_set_id(self):
+        """Test set id"""
+        self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
+
+
+class TestBase_to_json_string(unittest.TestCase):
+    """Unittests for ..."""
 
     def test_to_json_string_empty(self):
         """Test that the to_json_string method returns "[]" for an empty list"""
@@ -46,10 +63,34 @@ class TestBase(unittest.TestCase):
         expected_json = '[{"id": 1, "name": "John"}, {"id": 2, "name": "Jane"}]'
         self.assertEqual(json_string, expected_json)
 
+
+class TestBase_from_json_string(unittest.TestCase):
+    """Unittests for..."""
+
     def test_from_json_string_empty(self):
         """Test that the from_json_string method returns an empty list for an empty JSON"""
         list_objs = Base.from_json_string("[]")
         self.assertEqual(list_objs, [])
+
+
+class TestBase_load_from_file(unittest.TestCase):
+    """Unittests for..."""
+
+    def test_load_from_file_first_rectangle(self):
+        """Teste load from file rectangle"""
+        r1 = Rectangle(10, 7, 2, 8, 1)
+        r2 = Rectangle(2, 4, 5, 6, 2)
+        Rectangle.save_to_file([r1, r2])
+        list_rectangles_output = Rectangle.load_from_file()
+        self.assertEqual(str(r1), str(list_rectangles_output[0]))
+
+    def test_load_from_file_rectangle_types(self):
+        """Teste load from file rectangle"""
+        r1 = Rectangle(10, 7, 2, 8, 1)
+        r2 = Rectangle(2, 4, 5, 6, 2)
+        Rectangle.save_to_file([r1, r2])
+        output = Rectangle.load_from_file()
+        self.assertTrue(all(type(obj) == Rectangle for obj in output))
 
 
 if __name__ == "__main__":
