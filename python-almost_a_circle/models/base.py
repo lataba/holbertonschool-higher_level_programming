f0f1dffc6c178c4c57454c6d0d595c1fd5720326
root <ltballeste@gmail.com>
2023-07-02 13:48:27 -0700

    fixing to jason string adding an else

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,8 @@ class Base:
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
