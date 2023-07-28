9c640372ce62c905c1ff96de6692aa9be6adb3fd
lataba <ltballeste@gmail.com>
2023-07-28 12:35:41 -0700

    Task9, first version

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- Lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name FROM cities 
+    AS cities INNER JOIN states AS states
+        ON state_id = states.id ORDER BY cities.id;
