fa57a1e506859e696bd69cd263b5042445f7d18e
lataba <ltballeste@gmail.com>
2023-07-30 15:45:17 -0700

    Task3, first version

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""""
+Takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    connection = MySQLdb.connect(
+                    user=sys.argv[1],
+                    password=sys.argv[2],
+                    db=sys.argv[3])
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM states;")
+
+    states_data = cursor.fetchall()
+    [print(state) for state in states_data if state[1] == sys.argv[4]]
+
+    cursor.close()
+    connection.close()
