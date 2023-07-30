79983357bab5c6a083436012dbb3d54d73cf5952
lataba <ltballeste@gmail.com>
2023-07-30 09:55:56 -0700

    Task0, first version

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Module that lists all states from the database hbtn_0e_0_usa
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user_arg, password_arg, db_name_arg = sys.argv[1:4]
+    connection = MySQLdb.connect(user=user_arg, passwd=password_arg, db=db_name_arg)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `states`")
+    states_data = cursor.fetchall()
+    [print(state) for state in states_data]
