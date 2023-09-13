426bc0a65389ef366c95c5880cd58540963e65ad
lataba <ltballeste@gmail.com>
2023-09-12 22:05:27 -0700

    Task0

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,5 +1,5 @@
 // Select the header element
-let header = document.querySelector('header');
+const header = document.querySelector('header');
 
 // Update the text color of the header element to red
 header.style.color = '#FF0000';
